a01a335183bfbc6025bda25d39509bde393c73d6
cealach <cealach.oconnor@cattleeye.com>
2023-07-28 11:40:44 +0100

    chore: created display component

---
+++ src/components/Display.js
@@ -0,0 +1,21 @@
+import './Buttons'
+import Buttons from './Buttons';
+import './styles/Display.css'
+
+const Display = () => {
+  
+  const onButtonClick = (e) => {
+    function play(e) {
+      
+    }
+  }
+  
+  return (
+    <div>
+      <Buttons onButtonClick={onButtonClick}/>
+
+    </div>
+  )
+}
+
+export default Display;
